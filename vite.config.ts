a5868c0441597ce0610b6a39453a44e64cbd0cd7
leavion <gameacc44@yandex.ru>
2025-10-17 20:24:20 +0500

    gh pages actions setup

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/modulal-franchise",
   plugins: [vue(), vueJsx(), vueDevTools()],
   css: {
     preprocessorOptions: {
